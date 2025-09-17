0204973dd917b9ff25e3f4e8250532e76ed3da9b
davidy <david.yang026@gmail.com>
2025-09-17 14:43:32 -0700

    updated users

---
+++ wealth_path_backend/users/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+from django.contrib.auth.forms import UserCreationForm
+
+
+
+def index(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+    return render(request, 'users/user.html')
+
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+
+
+        if user:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "users/login.html", {
+                "message": "Invalid Credentials"
+            })    
+    return render(request, "users/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return render(request, "users/login.html",{
+        "message": "Logged Out"
+    })
+
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request,user)
+            return HttpResponseRedirect(reverse("index"))
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/register.html',{
+        "form":form
+    })
