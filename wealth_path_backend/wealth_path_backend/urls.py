af954a103a9ade8d00c7153136bff70b561cb08e
davidy <david.yang026@gmail.com>
2025-09-18 11:23:48 -0700

    transaction model serialized and routed

--- wealth_path_backend/wealth_path_backend/urls.py
+++ wealth_path_backend/wealth_path_backend/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from transactions.api.viewset import TransactionViewSet
+
+router = DefaultRouter()
+
+router.register(r"transactions", TransactionViewSet, basename="transaction")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls'))
+    path('users/', include('users.urls')),
+    path("api/", include(router.urls))
 ]
