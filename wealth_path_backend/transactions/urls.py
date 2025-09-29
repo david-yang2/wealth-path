20b1f318da71f4e95eb3e7bf420f84c9fc555a78
davidy <david.yang026@gmail.com>
2025-09-29 14:12:56 -0700

    updated path for user's transaction list

--- wealth_path_backend/transactions/urls.py
+++ wealth_path_backend/transactions/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.TransactionsListAPIView.as_view())
+    path("", views.UserTransactionsListAPIView.as_view())
 ]
--- wealth_path_backend/users/urls.py
+++ wealth_path_backend/users/urls.py
@@ -4,7 +4,7 @@ from transactions.views import UserTransactionsListAPIView
 
 urlpatterns=[
     # path("", views.index, name="index"),
-    path("", UserTransactionsListAPIView.as_view()),
+    # path("", UserTransactionsListAPIView.as_view()),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register_view, name="register")
