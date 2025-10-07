4e554241ae28567b1beef8a679ff78fb1aa0fc54
davidy <david.yang026@gmail.com>
2025-10-07 12:08:43 -0700

    initial tailwind setup

--- wealth_path_frontend/src/App.jsx
+++ wealth_path_frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter,Routes,Route} from "react-router-dom"
+import {BrowserRouter,Routes,Route, Link} from "react-router-dom"
 import Transactions from "./components/Transactions"
 import Login from "./components/Login"
 import HomePage from "./components/HomePage"
@@ -9,6 +9,11 @@ const App = () => {
 
   return (
     <BrowserRouter>
+      <nav className="flex flex-row justify-end">
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/transactions">Transactions</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
