881fc22448481875c70a29f21c7b8934d4639ce2
davidy <david.yang026@gmail.com>
2025-10-12 12:37:27 -0700

    adjusted position of title

--- wealth_path_frontend/src/components/NavBar.jsx
+++ wealth_path_frontend/src/components/NavBar.jsx
@@ -6,12 +6,12 @@ const NavBar = () => {
     const {user} = useAuth()
 
     return (
-    <div className="flex flex-row justify-between bg-slate-100  px-2 py-5">
-        <div className="text-xl">
+    <div className="flex flex-row justify-between bg-slate-100  px-2 py-5 h-24">
+        <div className="text-2xl font-bold flex items-center pl-5 border-2 border-red-200 w-1/3">
             Wealth Path
         </div>
         {user ? (
-        <nav className="flex flex-row justify-around border-2 border-red-200 w-1/3 text-l">
+        <nav className="flex flex-row justify-around items-center border-2 border-red-200 w-1/3 text-l">
             <Link to="/transactions">Transactions</Link>
             <Link to="/logout">Logout</Link>
         </nav>
