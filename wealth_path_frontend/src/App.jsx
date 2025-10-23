d18055b04bcd40468509519c62338b1ee9a1621f
davidy <david.yang026@gmail.com>
2025-10-23 09:56:01 -0700

    created useWindowWidth hook and Layout helper function to display NavBar only when screen size is above medium breakpoint and user is not on homepage

--- wealth_path_frontend/src/App.jsx
+++ wealth_path_frontend/src/App.jsx
@@ -1,4 +1,10 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useLocation,
+} from "react-router-dom";
 import Transactions from "./components/Transactions";
 import Login from "./components/Login";
 import Logout from "./components/Logout";
@@ -6,38 +12,52 @@ import HomePage from "./components/HomePage";
 import NavBar from "./components/NavBar";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import AuthProvider from "./components/auth/AuthContext";
+import useWindowWidth from "./components/useWindowWidth";
 
 const App = () => {
+  // Layout of paths and to call useLocation()
+  const Layout = () => {
+    const width = useWindowWidth();
+    const location = useLocation();
+
+    const showNavBar = width >= 768 && location !== "/";
+
+    return (
+      <div className="flex flex-col h-full w-full">
+        <div className="w-full flex justify-center">
+          {showNavBar && <NavBar />}
+        </div>
+        <div className="flex flex-1 justify-center">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="/transactions"
+              element={
+                <ProtectedRoute>
+                  <Transactions />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/logout"
+              element={
+                <ProtectedRoute>
+                  <Logout />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </div>
+      </div>
+    );
+  };
 
   return (
     <AuthProvider>
       <div className="flex flex-col h-full w-full">
         <BrowserRouter>
-          <div className="w-full flex justify-center">
-          <NavBar />
-          </div>
-          <div className="flex flex-1 justify-center">
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/login" element={<Login />} />
-              <Route
-                path="/transactions"
-                element={
-                  <ProtectedRoute>
-                    <Transactions />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/logout"
-                element={
-                  <ProtectedRoute>
-                    <Logout />
-                  </ProtectedRoute>
-                }
-              />
-            </Routes>
-          </div>
+          <Layout />
         </BrowserRouter>
       </div>
     </AuthProvider>
--- wealth_path_frontend/src/components/NavBar.jsx
+++ wealth_path_frontend/src/components/NavBar.jsx
@@ -7,12 +7,13 @@ const NavBar = () => {
   const { user } = useAuth();
 
   return (
-    <div className='bg-green-400 hidden w-full md:flex flex-row justify-between items-center px-[2.5%] py-5 mb-3'>
-      <div className="text-4xl font-bold flex items-center">
-        Wealth Path
-      </div>
+    <div className="bg-green-400 w-full flex flex-row justify-between items-center px-[2.5%] py-5 mb-3">
+      <div className="text-4xl font-bold flex items-center">Wealth Path</div>
       <div className="relative">
-        <i onClick={() => setOpenMenu(true)} className="fa-solid fa-bars text-4xl"></i>
+        <i
+          onClick={() => setOpenMenu(true)}
+          className="fa-solid fa-bars text-4xl"
+        ></i>
         {openMenu ? <NavModal user={user} setOpenMenu={setOpenMenu} /> : null}
       </div>
     </div>
