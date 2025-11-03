d8f06a580b6984252bc942abb8ec9a6cb011dc8f
davidy <david.yang026@gmail.com>
2025-11-03 09:44:02 -0800

    path for TransactionTotalsAPIView

--- wealth_path_backend/transactions/urls.py
+++ wealth_path_backend/transactions/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.UserTransactionsListAPIView.as_view(), name='transactions-list')
+    path("", views.UserTransactionsListAPIView.as_view(), name='transactions-list'),
+    path("totals", views.TransactionTotalsAPIView.as_view(), name="totals")
 ]
