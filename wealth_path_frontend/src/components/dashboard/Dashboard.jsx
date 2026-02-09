164eae6469ac0bcb52e1a87fab0a78906818eacb
davidy <david.yang026@gmail.com>
2026-02-09 13:42:04 -0800

    allow users to toggle between Monthly YTD and Inception balances as well as restyled

--- wealth_path_frontend/src/components/dashboard/Dashboard.jsx
+++ wealth_path_frontend/src/components/dashboard/Dashboard.jsx
@@ -2,20 +2,46 @@ import BudgetGoals from "./BudgetGoals";
 import QuickActions from "./QuickActions";
 import SpendingInsights from "./SpendingInsights";
 import Summary from "./Summary";
+import DashboardTransaction from "./DashboardTransaction";
 
 const Dashboard = () => {
+  // current date
+
+  const currentDate = new Date();
+
+  const year = currentDate.getFullYear();
+  const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+  const day = String(currentDate.getDate()).padStart(2, "0");
+
+  const currentDateStr = `${year}-${month}-${day}`
+
   return (
-    <div id="dashboard-container" className="w-full max-w-6xl">
-      <div className="text-3xl font-bold my-3 pl-[2.5%]">Dashboard</div>
-      <div className="min-h-screen w-full bg-gray-50 p-6 grid gap-6 md:grid-cols-2 rounded-lg">
-        <div className="md:col-span-2">
-          <Summary />
+    <div
+      id="dashboard-container"
+      className="w-full min-h-screen flex justify-center bg-gray-50 px-6 py-8"
+    >
+      <div className="w-full max-w-7xl flex flex-col gap-8">
+        {/* header */}
+        <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-2">
+          <h1 className="text-3xl font-bold text-gray-900">Dashboard</h1>
+
+          <span className="text-sm text-gray-500">
+            Today • {currentDateStr}
+          </span>
         </div>
 
-        {/* Row 2 */}
-        <SpendingInsights />
-        <BudgetGoals />
-        <QuickActions />
+        {/* components */}
+        <div className=" w-full p-6 grid gap-6 grid-cols-1 md:grid-cols-2 rounded-2xl">
+          <Summary currentDateStr={currentDateStr} year={year} month={month}/>
+          <SpendingInsights year={year} month={month} day={day} />
+
+          {/* Row 2 */}
+          <div className="col-span-1 md:col-span-2 overflow-auto">
+            <DashboardTransaction />
+          </div>
+          {/* <BudgetGoals />
+        <QuickActions /> */}
+        </div>
       </div>
     </div>
   );
--- wealth_path_frontend/src/components/dashboard/Summary.jsx
+++ wealth_path_frontend/src/components/dashboard/Summary.jsx
@@ -2,35 +2,45 @@ import DashboardCard from "./DashboardCard";
 import { useState, useEffect } from "react";
 import { getTotals } from "../transactionsApi";
 
-const Summary = () => {
+const Summary = (props) => {
   const [totalIncome, setTotalIncome] = useState(0);
   const [totalExpense, setTotalExpense] = useState(0);
+  const [balanceStartDate, setBalanceStartDate] = useState();
+  const [selectedReturn, setSelectedReturn] = useState("Monthly");
+
+  const { currentDateStr, year, month } = props;
 
   useEffect(() => {
-    getTotals()
+    getTotals(balanceStartDate, currentDateStr)
       .then((data) => {
         setTotalIncome(data.total_income);
         setTotalExpense(data.total_expense);
       })
       .catch((err) => console.error(err));
-  }, []);
+  }, [currentDateStr, balanceStartDate]);
 
-  const savings = totalIncome - totalExpense;
-  const expenseRatio = (totalExpense / totalIncome) * 100;
-  const percentage = (savings / totalIncome) * 100;
+  const savings = (totalIncome - totalExpense).toFixed(2);
+  const expenseRatio = ((totalExpense / totalIncome) * 100).toFixed(2);
+  const percentage = ((savings / totalIncome) * 100).toFixed(2);
 
+  // style for selected time frame
+  const timeFrameStyle = "bg-slate-300 px-2 py-2 rounded-md";
   return (
     <DashboardCard>
-      <div className="flex flex-row h-full justify-between items-center px-5 ">
-        <div className="flex flex-col flex-1 h-full justify-between ">
-          <div className="text-2xl font-bold mb-3"> Summary</div>
-          <div className="mb-3"> Total income: ${Number(totalIncome).toLocaleString()}</div>
-          <div className="mb-3"> Total expense: ${Number(totalExpense).toLocaleString()}</div>
+      <div className="flex flex-col h-full justify-start px-5 ">
+        {/* account info */}
+        <div className="flex justify-start mb-10">
+          <div className="text-xl mb-3 mr-10 underline"> Account Balance: </div>
+          <div className="text-xl font-bold ">
+            ${Number(savings).toLocaleString()}
+          </div>
         </div>
-        {/* circular progress */}
-        <div className="w-[50%]  flex flex-col justify-end items-center">
+
+        {/* content container */}
+        <div className="w-full flex flex-row justify-between">
+          {/* graph - circular progress */}
           <div
-            className="w-[75%] aspect-square rounded-full flex items-center justify-center mb-3"
+            className="w-[50%] aspect-square rounded-full flex items-center justify-center mb-3"
             style={{
               background: `conic-gradient(#FFA500 ${
                 expenseRatio * 3.6
@@ -38,25 +48,76 @@ const Summary = () => {
             }}
           >
             {/* Inner circle to make it look like a ring */}
-            <div className="w-[70%] h-[70%] bg-white rounded-full flex flex-col items-center justify-center">
-              <span className="text-xl font-bold text-gray-800">Total Savings:</span>
-              <span className="text-sm text-gray-500">${savings}</span>
+            <div className="w-[70%] h-[70%] aspec-square bg-gray-50 rounded-full flex flex-col items-center justify-center">
+              {/* <span className="text-l items-center text-center text-gray-800">
+                  Total Savings:
+                </span>
+                <span className="text-sm text-gray-500">
+                  ${Number(savings).toLocaleString()}
+                </span> */}
             </div>
           </div>
-          <div id="legends" className="flex flex-row">
-            <div className="flex flex-row mr-5">
+
+          {/* time frame and label */}
+          <div className="flex flex-col ml-6 justify-between" id="balance-content">
+            <div className="flex flex-col justify-start">
               <div
-                className="w-5 aspect-square rounded-2xl mr-1"
-                style={{ backgroundColor: "#FFA500" }}
-              ></div>
-              <div>Expense</div>
+                id="balance-selection"
+                className="flex flex-row justify-between items-center font-bold mb-4"
+              >
+                <div
+                  onClick={() => {
+                    setBalanceStartDate(`${year}-${month}-01`);
+                    setSelectedReturn("Monthly");
+                  }}
+                  className={selectedReturn === "Monthly" && timeFrameStyle}
+                >
+                  Monthly
+                </div>
+                <div
+                  onClick={() => {
+                    setBalanceStartDate(`${year}-01-01`);
+                    setSelectedReturn("YTD");
+                  }}
+                  className={selectedReturn === "YTD" && timeFrameStyle}
+                >
+                  YTD
+                </div>
+                <div
+                  onClick={() => {
+                    setBalanceStartDate(`2001-01-01`);
+                    setSelectedReturn("Inception");
+                  }}
+                  className={selectedReturn === "Inception" && timeFrameStyle}
+                >
+                  Inception
+                </div>
+              </div>
+              <div className="text-md mb-3">
+                {" "}
+                Total income: ${Number(totalIncome).toLocaleString()}
+              </div>
+              <div className="text-md mb-3">
+                {" "}
+                Total expense: ${Number(totalExpense).toLocaleString()}
+              </div>
             </div>
-            <div className="flex flex-row">
-              <div
-                className="w-5 aspect-square rounded-2xl mr-1"
-                style={{ backgroundColor: "#41dc8e" }}
-              ></div>
-              <div>Savings</div>
+            {/* legend */}
+            <div id="legends" className="flex flex-row">
+              <div className="flex flex-row mr-5">
+                <div
+                  className="w-5 aspect-square rounded-2xl mr-1"
+                  style={{ backgroundColor: "#FFA500" }}
+                ></div>
+                <div>Expense</div>
+              </div>
+              <div className="flex flex-row">
+                <div
+                  className="w-5 aspect-square rounded-2xl mr-1"
+                  style={{ backgroundColor: "#41dc8e" }}
+                ></div>
+                <div>Savings</div>
+              </div>
             </div>
           </div>
         </div>
